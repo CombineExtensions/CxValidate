4d22742bd05577845469a723a12933c7dd6aab58
Steven Sherry <steven.sherry@affinityforapps.com>
2019-10-24 22:53:11 -0500

    Renamed package to CxValidate. Added documentation and tests. Added github workflows and jazzy config.

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
       targets: ["CxValidate"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/CombineExtensions/CxTest.git", from: "0.1.0"),
+    .package(url: "https://github.com/CombineExtensions/CxDisposeBag.git", from: "0.1.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,6 +29,6 @@ let package = Package(
       dependencies: []),
     .testTarget(
       name: "CxValidateTests",
-      dependencies: ["CxValidate"]),
+      dependencies: ["CxValidate", "CxTest", "CxDisposeBag"]),
   ]
 )
--- Sources/CxValidate/CxValidate.swift
+++ Sources/CxValidate/CxValidate.swift
@@ -2,6 +2,8 @@ import Combine
 import Foundation
 
 extension Publisher where Output == (data: Data, response: URLResponse) {
+  /// Validates a successful `(200...299)` response status code from an HTTP response and forwards the response `Data`.
+  /// On failure, it sends a `URLError` with  code `.badServerResponse`
   public func validate() -> Publishers.TryMap<Self, Data> {
     tryMap { data, response in
       guard
